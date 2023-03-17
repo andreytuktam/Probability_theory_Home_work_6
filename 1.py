16bfa994c7730ab2b530c89f455bca014b5ad1c5
andreytuktam <107698587+andreytuktam@users.noreply.github.com>
2023-03-18 00:01:03 +0700

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,4 +1,4 @@
-import numpy as np 
-
-mean_std = np.sqrt(16) / 256
-print(mean_std)
+import math
+import scipy.stats as stats
+t1 = stats.t.ppf (0.975, 255)
+print(80 - t1 * (16 / math.sqrt(256)), ";", 80 + t1 * (16 / math.sqrt(256)))
